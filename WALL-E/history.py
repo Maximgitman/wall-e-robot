76874649d5b17b90f7c7c14ac7e3f301ca2f9af8
Aleksey Teplyakov <alekseytep92@gmail.com>
2022-05-29 01:26:59 +0300

    Updated class History in module history.py

--- WALL-E/history.py
+++ WALL-E/history.py
@@ -16,55 +16,59 @@ class History(object):
                       2: (1, 0),   # down
                       3: (0, -1),  # left
                       4: (0, 1)}   # right
+        self.prev_obstacles = None
+        self.prev_actions = None
 
     def update_history(self, obstacles, actions):
-        actions = np.array(list(map(lambda x: self.steps[x], actions)))
+        if self.prev_actions is not None:
+            mask = [np.equal(self.prev_obstacles[i], obstacles[i]) for i in range(len(obstacles))]
+            self.prev_actions *= mask
 
-        # обновляем позиции относительно старта и текущие позиции на карте
-        self.relative_pos += actions
-        self.curr_pos += actions
+            # обновляем позиции относительно старта и текущие позиции на карте
+            self.relative_pos += self.prev_actions
+            self.curr_pos += self.prev_actions
 
-        for bot, obs in enumerate(obstacles):
-            min_up, max_down, min_left, max_right = self.edges[bot]
+            for bot, obs in enumerate(obstacles):
+                min_up, max_down, min_left, max_right = self.edges[bot]
 
-            # проверяеям выход за верхний предел для агента
-            if self.relative_pos[bot, 0] < min_up:
-                new_line = np.empty((1, self.maps[bot].shape[1]))
-                new_line[:] = np.NaN
-                self.maps[bot] = np.concatenate((new_line, self.maps[bot]), axis=0)
-                min_up = self.relative_pos[bot, 0]
-                self.curr_pos[bot, 0] += 1
+                # проверяеям выход за верхний предел для агента
+                if self.relative_pos[bot, 0] < min_up:
+                    new_line = np.empty((1, self.maps[bot].shape[1]))
+                    new_line[:] = np.NaN
+                    self.maps[bot] = np.concatenate((new_line, self.maps[bot]), axis=0)
+                    min_up = self.relative_pos[bot, 0]
+                    self.curr_pos[bot, 0] += 1
 
-            # проверяеям выход за нижний предел для агента
-            elif self.relative_pos[bot, 0] > max_down:
-                new_line = np.empty((1, self.maps[bot].shape[1]))
-                new_line[:] = np.NaN
-                self.maps[bot] = np.concatenate((self.maps[bot], new_line), axis=0)
-                max_down = self.relative_pos[bot, 0]
+                # проверяеям выход за нижний предел для агента
+                elif self.relative_pos[bot, 0] > max_down:
+                    new_line = np.empty((1, self.maps[bot].shape[1]))
+                    new_line[:] = np.NaN
+                    self.maps[bot] = np.concatenate((self.maps[bot], new_line), axis=0)
+                    max_down = self.relative_pos[bot, 0]
 
-            # проверяеям выход за левый предел для агента
-            if self.relative_pos[bot, 1] < min_left:
-                new_col = np.empty((self.maps[bot].shape[0], 1))
-                new_col[:] = np.NaN
-                self.maps[bot] = np.concatenate((new_col, self.maps[bot]), axis=1)
-                min_left = self.relative_pos[bot, 1]
-                self.curr_pos[bot, 1] += 1
+                # проверяеям выход за левый предел для агента
+                if self.relative_pos[bot, 1] < min_left:
+                    new_col = np.empty((self.maps[bot].shape[0], 1))
+                    new_col[:] = np.NaN
+                    self.maps[bot] = np.concatenate((new_col, self.maps[bot]), axis=1)
+                    min_left = self.relative_pos[bot, 1]
+                    self.curr_pos[bot, 1] += 1
 
-            # проверяеям выход за правый предел для агента
-            elif self.relative_pos[bot, 1] > max_right:
-                new_col = np.empty((self.maps[bot].shape[0], 1))
-                new_col[:] = np.NaN
-                self.maps[bot] = np.concatenate((self.maps[bot], new_col), axis=1)
-                max_right = self.relative_pos[bot, 1]
+                # проверяеям выход за правый предел для агента
+                elif self.relative_pos[bot, 1] > max_right:
+                    new_col = np.empty((self.maps[bot].shape[0], 1))
+                    new_col[:] = np.NaN
+                    self.maps[bot] = np.concatenate((self.maps[bot], new_col), axis=1)
+                    max_right = self.relative_pos[bot, 1]
 
-            # обновляем пределы для агента
-            self.edges[bot] = np.array((min_up, max_down, min_left, max_right))
+                # обновляем пределы для агента
+                self.edges[bot] = np.array((min_up, max_down, min_left, max_right))
 
-            # обновляем карту агента
-            x, y = self.curr_pos[bot]
-            self.maps[bot][x-5:x+6, y-5:y+6] = obs
-
-        return self.maps
+                # обновляем карту агента
+                x, y = self.curr_pos[bot]
+                self.maps[bot][x-5:x+6, y-5:y+6] = obs
+        self.prev_obstacles = obstacles
+        self.prev_actions = np.array(list(map(lambda x: self.steps[x], actions)))
 
 
 if __name__ == '__main__':
